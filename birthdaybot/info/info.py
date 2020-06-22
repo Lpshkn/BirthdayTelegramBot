849e14782735f5697d3d0b2f6906103492f99994
Nikshepel <kofffsh@gmail.com>
2020-06-23 02:46:34 +0300

    Add information when the bot will be stopped, moved all files to the certain language directory
    All information files will be moved into the certain language directory and will be used next depending on the language that a user speaks.

--- birthdaybot/info/info.py
+++ birthdaybot/info/info.py
@@ -2,9 +2,15 @@
 Module for printing and processing all the information about the bot or about actions with this bot
 """
 import codecs
+import os
+
+# Define the language directory containing information in the certain language
+ENGLISH = "en"
+LANGUAGE_DIRECTORY = os.path.join("./info", ENGLISH)
 
 # Define all the filenames of HTML info files
-START_INFO = "./info/start_info.html"
+START_INFO = os.path.join(LANGUAGE_DIRECTORY, "start_info.html")
+STOP_BOT_INFO = os.path.join(LANGUAGE_DIRECTORY, "stop_bot_info.html")
 
 
 def get_info(filename: str) -> str:
