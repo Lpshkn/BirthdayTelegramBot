d71d2ec5272835758e5df8e3201312d38d989061
Nikshepel <kofffsh@gmail.com>
2020-06-26 01:41:41 +0300

    Add a new handler to check all text messages in the main menu

--- birthdaybot/handlers.py
+++ birthdaybot/handlers.py
@@ -5,7 +5,6 @@ import telegram
 import birthdaybot.menus as menus
 from birthdaybot.localization import localization
 from telegram.ext import Updater
-from birthdaybot.localization.localization import Localization
 
 # Define all the states of the bot
 MAIN_MENU = range(1)
@@ -16,13 +15,11 @@ def start_handler(update: telegram.Update, context: telegram.ext.CallbackContext
     The handler to /start command. This handler send an localization message to the user.
     """
     code = update.effective_user.language_code
-    Localization.set_language_code(code)
-
     context.bot.sendMessage(chat_id=update.effective_chat.id,
-                            text=localization.Localization.get_info(Localization.START_INFO),
+                            text=localization.start_info(code),
                             parse_mode=telegram.ParseMode.HTML,
                             disable_web_page_preview=True,
-                            reply_markup=menus.get_main_menu())
+                            reply_markup=menus.get_main_menu(code))
 
     return MAIN_MENU
 
@@ -31,10 +28,30 @@ def stop_bot_handler(update: telegram.Update, context: telegram.ext.CallbackCont
     """
     The handler to /stop command. This handler stops the bot and remove all data about the user
     """
+    code = update.effective_user.language_code
     context.bot.sendMessage(chat_id=update.effective_chat.id,
-                            text=localization.Localization.get_info(Localization.STOP_BOT_INFO),
+                            text=localization.stop_bot_info(code),
                             parse_mode=telegram.ParseMode.HTML,
                             disable_web_page_preview=True,
                             reply_markup=menus.remove_keyboard())
 
     return telegram.ext.ConversationHandler.END
+
+
+def main_menu_handler(update: telegram.Update, context: telegram.ext.CallbackContext):
+    """
+    The handler for the main menu. This handler process commands for the main menu.
+    """
+    # Get language code and get the dictionary of the main menu
+    code = update.effective_user.language_code
+    main_menu = localization.main_menu(code)
+
+    # Get the text of the message and compare it with the name of buttons
+    text = update.message.text
+    if text == main_menu[menus.STOP_BOT_BUTTON]:
+        context.bot.sendMessage(chat_id=update.effective_chat.id,
+                                text=localization.stop_bot_info(code),
+                                parse_mode=telegram.ParseMode.HTML,
+                                disable_web_page_preview=True,
+                                reply_markup=menus.remove_keyboard())
+        return telegram.ext.ConversationHandler.END
