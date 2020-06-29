626bd8d2ad45df5207575fa2d43e5b3dc4cdaf58
Nikshepel <kofffsh@gmail.com>
2020-06-29 21:36:06 +0300

    Add methods in the Database module to connect to the database and create table if it's necessary

--- birthdaybot/db/database.py
+++ birthdaybot/db/database.py
@@ -1,6 +1,5 @@
 import psycopg2
-import birthdaybot.configurator as cfg
-from os.path import join
+import sys
 
 
 class Database:
@@ -10,3 +9,37 @@ class Database:
         self.password = json_obj["PG_PSWD"]
         self.host = json_obj["PG_HOST"]
         self.port = json_obj["PG_PORT"]
+
+        self.connection = self.connect()
+        self.create_tables()
+
+    def connect(self):
+        try:
+            connection = psycopg2.connect(database=self.name,
+                                          user=self.user,
+                                          password=self.password,
+                                          host=self.host,
+                                          port=self.port)
+
+            return connection
+        except psycopg2.OperationalError as e:
+            print("Error: {}".format(e), file=sys.stderr)
+            exit(-2)
+
+    def create_tables(self):
+        with self.connection.cursor() as cur:
+            self.connection.autocommit = True
+
+            cur.execute("CREATE TABLE IF NOT EXISTS Chats ("
+                        "chat_id integer CONSTRAINT chats_pkey PRIMARY KEY,"
+                        "conversation_state varchar(64) NOT NULL);")
+
+            cur.execute("CREATE TABLE IF NOT EXISTS Users ("
+                        "user_id integer CONSTRAINT users_pkey PRIMARY KEY,"
+                        "chat_id integer REFERENCES Chats ON DELETE SET NULL ON UPDATE CASCADE,"
+                        "username varchar(32) NOT NULL UNIQUE);")
+
+            cur.execute("CREATE TABLE IF NOT EXISTS Notes ("
+                        "note_id integer CONSTRAINT notes_pkey PRIMARY KEY,"
+                        "chat_id integer REFERENCES Chats ON DELETE CASCADE ON UPDATE CASCADE,"
+                        "datetime timestamp(0) NOT NULL UNIQUE);")
--- birthdaybot/main.py
+++ birthdaybot/main.py
@@ -1,10 +1,20 @@
 import sys
 from birthdaybot.configurator import Configurator
 from birthdaybot.bot import BirthdayBot
+from birthdaybot.db.database import Database
+
+import psycopg2
 
 
 def main():
     configurator = Configurator(sys.argv[1:])
+
+    try:
+        database = Database(configurator.get_dbconfig())
+    except KeyError as e:
+        print("Error: Undefined parameter {}".format(e), file=sys.stderr)
+        exit(-1)
+
     bot = BirthdayBot(configurator.get_token(), configurator.get_persistence())
     bot.run()
 
