2cab3ece0c2292e2a06aa3d6bc8051b3f52a163a
Nikshepel <kofffsh@gmail.com>
2020-06-30 21:59:08 +0300

    Add a method to update the conversations into the Database module
    This method will update the conversations table of the database using a dictionary containing the chat's id and its state.

--- birthdaybot/db/database.py
+++ birthdaybot/db/database.py
@@ -55,6 +55,34 @@ class Database:
                         "chat_id integer REFERENCES Chats ON DELETE CASCADE ON UPDATE CASCADE,"
                         "datetime timestamp(0) NOT NULL UNIQUE);")
 
+    def update_conversation(self, conversations: dict, name: str = None, key: tuple = None):
+        """
+        Method updates a conversation's state in the database.
+
+        :param name: the name of a Conversation handler
+        :param conversations: a dictionary containing chat_id and conversation state
+        :param key: chat_id
+        """
+        self.add_chats(conversations, name)
+
+        if name and key:
+            chat_id = key[0]
+            with self.connection.cursor() as cur:
+                query = sql.SQL("INSERT INTO conversations(chat_id, {0}) VALUES ({1}, {2})"
+                                "ON CONFLICT (chat_id) DO UPDATE SET {0} = excluded.{0}").format(
+                    sql.Identifier(name), sql.Literal(chat_id), sql.Literal(conversations[name][key])
+                )
+                cur.execute(query)
+        else:
+            with self.connection.cursor() as cur:
+                for key, value in conversations["main_menu_state"].items():
+                    chat_id = key[0]
+                    query = sql.SQL("INSERT INTO conversations(chat_id, main_menu_state) VALUES ({0}, {1})"
+                                    "ON CONFLICT (chat_id) DO UPDATE SET main_menu_state = excluded.main_menu_state").format(
+                        sql.Literal(chat_id), sql.Literal(value)
+                    )
+                    cur.execute(query)
+
     def add_chats(self, conversations: dict, name: str = None):
         """
         Method to add chat_ids into the database.
